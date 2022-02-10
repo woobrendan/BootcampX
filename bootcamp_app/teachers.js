87d7a2316ca7f2040cf0ee6e7d3ad179bf897e8a
Brendan Woo <woo.brendan@gmail.com>
2022-02-10 01:32:11 +0000

    update queries to use parameterized queries to prevent SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,19 @@ const pool = new Pool ({
   database: 'bootcampx'
 });
 
-
-
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohorts
 FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teachers.name;
-`)
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name;`;
+
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`]
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(teacher => {
     console.log(`${teacher.cohorts}: ${teacher.teacher}`);
